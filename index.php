1dd9f3a34acbc435f7dd81cec5d42c2c47586245
dumanov96 <dumanov.teodor96@gmail.com>
2019-02-01 20:39:52 +0200

    index.php file - done

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Simple login form</title>
+  </head>
+  <body>
+    <div class="log-form">
+      <h2>Login to your account</h2>
+    <form>
+      <input type="text" name="username" placeholder="username" />
+      <input type="password" name="password" placeholder="password" />
+      <button type="submit" class="btn">Login</button>
+    </form>
+    </div>
+  </body>
+</html>
