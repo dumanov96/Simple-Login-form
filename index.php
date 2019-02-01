2dd1ee1f02fd8223b77583910113f79cb227ab53
dumanov96 <dumanov.teodor96@gmail.com>
2019-02-01 21:07:57 +0200

    styles.css done

--- index.php
+++ index.php
@@ -3,13 +3,14 @@
   <head>
     <meta charset="utf-8">
     <title>Simple login form</title>
+    <link rel="stylesheet" href="styles.css">
   </head>
   <body>
     <div class="log-form">
       <h2>Login to your account</h2>
     <form>
-      <input type="text" name="username" placeholder="username" />
-      <input type="password" name="password" placeholder="password" />
+      <input type="text" name="username" class="username" placeholder="username" />
+      <input type="password" name="password" class="password" placeholder="password" />
       <button type="submit" class="btn">Login</button>
     </form>
     </div>
